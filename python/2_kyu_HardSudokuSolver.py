577a7f2b816fec22502be87fb9b7822d8ae8dcef
Peter Liang <peter_liang@outlook.com>
2018-05-20 21:05:19 +0800

    Updated performance

--- python/2_kyu_HardSudokuSolver.py
+++ python/2_kyu_HardSudokuSolver.py
@@ -15,33 +15,83 @@ def sudoku_solver(puzzle):
     if 81 - zeros < 17:
         raise Exception()
 
-    for r in range(len(puzzle)):
-        for c in range(len(puzzle[r])):
-            if puzzle[r][c] != 0:
-                continue
-            for num in range(1, 10):
-                if check_num(puzzle, r, c, num):
-                    puzzle[r][c] = num
-                    if not sudoku_solver(puzzle):
-                        continue
+    get_all_candidates(puzzle)
 
-                    return puzzle
-            puzzle[r][c] = 0
+    return try_all_candidates(puzzle)
+
+
+def try_all_candidates(puzzle):
+    for r in range(9):
+        for c in range(9):
+            num = puzzle[r][c]
+            if is_fix_num(num):
+                continue
+            for i in num:
+                if not check_num(puzzle, r, c, i):
+                    continue
+                puzzle[r][c] = i
+                cleanup_candidates(puzzle)
+                get_all_candidates(puzzle)
+                try:
+                    if not try_all_candidates(puzzle):
+                        continue
+                except ValueError:
+                    continue
+
+                return puzzle
+            puzzle[r][c] = num
             return False
     return puzzle
 
 
+def cleanup_candidates(puzzle):
+    for r in range(9):
+        for c in range(9):
+            if type(puzzle[r][c]) is list:
+                puzzle[r][c] = []
+
+
+def get_all_candidates(puzzle):
+    for r in range(9):
+        for c in range(9):
+            num = puzzle[r][c]
+            if is_fix_num(num):
+                continue
+            candidates = []
+            for i in range(1, 10):
+                if check_num(puzzle, r, c, i):
+                    candidates.append(i)
+            if len(candidates) == 0:
+                raise ValueError()
+            puzzle[r][c] = candidates
+            if len(candidates) == 1:
+                get_all_candidates(puzzle)
+
+
+def is_fix_num(num):
+    return (isinstance(num, int) and num != 0) or (isinstance(num, list) and len(num) == 1)
+
+
+def is_one_num_list(num):
+    return type(num) is list and len(num) == 1
+
+
+def is_equal_to_num_or_candidate(num, candidate):
+    return num == candidate or (is_one_num_list(num) and num[0] == candidate)
+
+
 def check_num(puzzle, row, col, num):
-    if num in puzzle[row]:
-        return False
+    for i in puzzle[row]:
+        if is_equal_to_num_or_candidate(i, num):
+            return False
 
     for r in puzzle:
-        if num == r[col]:
+        if is_equal_to_num_or_candidate(r[col], num):
             return False
 
     for r in get_range(row):
         for c in get_range(col):
-            if num == puzzle[r][c]:
+            if is_equal_to_num_or_candidate(puzzle[r][c], num):
                 return False
 
     return True
